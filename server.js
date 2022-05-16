45de7fb0d1f3d415b4f96c0ef53e0b9287eb346c
Swagat Dash <swagatdash@Swagats-MacBook-Air.local>
2022-05-16 21:24:25 +0100

    heroku first commit

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ import express from 'express';
 import apiRouter from './api';
 import mongoose from 'mongoose';
 
+const cors = require("cors");
 const server = express();
+server.use(cors());
 const prt = process.env.PORT;
 const DB = process.env.DATABASE;
 
@@ -11,6 +13,13 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use('/api',apiRouter);
 server.use(express.static('public'));
+
+// Step 1:
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+server.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./public", "index.html"));
+});
 
 mongoose.connect(DB,{
     useNewUrlParser:true,
